435d915a35fa6fa9d5d3e5c4d49c96556fdf0914
AhmedAshrafghonemy <AhmedAshrafghonemy@gmail.com>
2021-12-12 00:41:04 +0200

    Initial Commit

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by macbook on 06/12/2021.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+   
+    var body: some View {
+        Text("hello world")
+    }
+
+
+    
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+}
---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by macbook on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
